d6cfac79a45fd65a111bbdad736abc7a9964c1e3
asaifur <automocascar@gmail.com>
2026-05-02 17:53:08 +0700

    domain_helper

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -9,6 +9,7 @@ class Admin extends MY_Controller
         parent::__construct();
         $this->load->model('User_model'); // Load model
         $this->load->helper('tanggal_helper');
+        $this->load->helper('domain_helper');
     }
 
     public function index()
